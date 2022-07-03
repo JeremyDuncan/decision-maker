4ddc9b58213478d9b8b321f366047f64997dbb4b
Jeremy Duncan <jeremy.duncan1984@gmail.com>
2022-07-03 18:25:47 -0400

    added state objects

--- decision-maker/src/components/Decisions.js
+++ decision-maker/src/components/Decisions.js
@@ -3,16 +3,41 @@ class Decisions extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      stateObject: "Object",
+      choices: [
+        "Mexican", 
+        "Chinese", 
+        "Vietmanese", 
+        "Thai", 
+        "Japanese", 
+        "Indian", 
+        "Brewery", 
+        "Italian", 
+        "American", 
+        "Mediterranean"
+      ],
+      mexican: [],
+      chinese: [],
+      vietmanese: [],
+      thai: [],
+      japanese: [],
+      indian: [],
+      brewery: [],
+      italian: [],
+      american: [],
+      mediterranean: []
     }
   }
 
   // Functions...
-    
-  }
+   handleClick = () => { 
+   alert(this.state.choices[2])
+  } 
+  
   render() {
     return(
       <div>
+        <h2>Decisions Component</h2>
+        <button onClick={() => { this.handleClick() }}>Press Generate Random Selection</button>
 
       </div>
     )
