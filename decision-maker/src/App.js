093f46f44c331660ee6c66ad8033142dbdd7e2e9
Jeremy Duncan <jeremy.duncan1984@gmail.com>
2022-07-03 17:56:09 -0400

    added bones

--- decision-maker/src/App.js
+++ decision-maker/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
+//import Module from './components/Module'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props) 
+    this.state = {
+      stateObject: "Object"
+    }
+  }
+  
+  // Function Declare area..
+  // function = () => { ... }
+
+  render(){
+    return(
+      <div className="App">
+          {/*// User input box*/}
+          <div>
+            <h1><strong>Decision Making App</strong></h1>
+          </div>
+          {/*<Module stateObject={this.state.stateObject}/>*/}
+      </div>
+    )
+  }
 }
 
 export default App;
