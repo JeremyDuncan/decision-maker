479c02907f7b382d26854ed2a0122f192b61bcd3
Jeremy Duncan <jeremy.duncan1984@gmail.com>
2022-07-03 18:05:13 -0400

    Created Decisions component

---
+++ decision-maker/src/components/Decisions.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+class Decisions extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      stateObject: "Object",
+    }
+  }
+
+  // Functions...
+    
+  }
+  render() {
+    return(
+      <div>
+
+      </div>
+    )
+  }
+}
+export default Decisions;
