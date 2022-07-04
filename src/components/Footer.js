d7950f1f811ab06b627eb8243f703efb8735484e
Jeremy Duncan <jeremy.duncan1984@gmail.com>
2022-07-04 13:49:12 -0400

    added Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+class Footer extends Component {
+
+  render() {
+    return (
+      <div>
+
+        <div className="footer">
+          <div id="name">Jeremy Duncan &copy; 2022</div>
+          <div id="page"><a href="https://github.com/JeremyDuncan">Jeremy's GitHub</a> </div>
+        </div>
+
+      </div>
+    )
+  }
+}
+export default Footer;
