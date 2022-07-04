cf03bd7950368f21c830667d1229e8df9065ce4c
Jeremy Duncan <jeremy.duncan1984@gmail.com>
2022-07-04 12:50:51 -0400

    updated links array

--- decision-maker/src/components/Decisions.js
+++ decision-maker/src/components/Decisions.js
@@ -109,7 +109,7 @@ class Decisions extends Component {
         "Atlanta Breakfast Club",
         "Sun in My Belly",
         "Leon’s Full Service",
-        "Argosy",
+        "Farm Burger",
         "Folk Art Restaurant"
       ],
       // Mediterranean
@@ -154,73 +154,73 @@ class Decisions extends Component {
       ],
       // Thai
       [
-        "https://26thai.com/",
-        "SriThai Thai Kitchen & Sushi Bar -Emory",
-        "Little Bangkok",
-        "Bangkok Station",
-        "Tuk Tuk Thai Food Loft"
+        <a href="https://26thai.com/">Visit Website</a>,
+        <a href="https://www.srithaikitchen.com/">Visit Website</a>,
+        <a href="http://littlebangkokatlanta.com/">Visit Website</a>,
+        <a href="https://bangkokstationthaifood.com/">Visit Website</a>,
+        <a href="https://www.tuktukatl.com/">Visit Website</a>
       ],
       // Japanese
       [
-        "JINYA Ramen Bar",
-        "Eight Sushi Lounge",
-        "Shoya Izakaya",
-        "Nakato Japanese Restaurant",
-        "Umi",
-        "Wagaya"
+        <a href="https://www.jinyaramenbar.com/">Visit Website</a>,
+        <a href="https://eightsushiatl.com/">Visit Website</a>,
+        <a href="https://shoya-izakaya.com/">Visit Website</a>,
+        <a href="https://www.nakatorestaurant.com/">Visit Website</a>,
+        <a href="https://umiatlanta.com/">Visit Website</a>,
+        <a href="https://www.wagayaatlanta.com/">Visit Website</a>
       ],
       // Indian
       [
-        "Desie Spice",
-        "Chai Pani",
-        "Purnima Bangladeshi Cuisine",
-        "Zyka",
-        "Cafe Bombay",
-        "Masti fun Indian Street Food",
-        "Tabla",
-        "Aamar Indian Cuisine"
+        <a href="https://desispiceindianatlanta.com/">Visit Website</a>,
+        <a href="https://www.chaipanidecatur.com/">Visit Website</a>,
+        <a href="https://www.purnimabangladeshi.com/">Visit Website</a>,
+        <a href="https://zyka.com/">Visit Website</a>,
+        <a href="https://cafebombayatlanta.com/">Visit Website</a>,
+        <a href="http://www.mastiatlanta.com/">Visit Website</a>,
+        <a href="https://tablaatlanta.com/">Visit Website</a>,
+        <a href="https://www.aamarindianatlanta.com/">Visit Website</a>
       ],
       // Brewery
       [
-        "SweetWater Brewing Company",
-        "New Realm Brewing",
-        "Hopstix",
-        "Bold Monk",
-        "The Porter Beer Bar",
-        "Wrecking Bar Brewpub",
-        "Torched Hop Brewing",
-        "Monday Night Brewing"
+        <a href="https://www.sweetwaterbrew.com/">Visit Website</a>,
+        <a href="https://newrealmbrewing.com/">Visit Website</a>,
+        <a href="https://hopstix.com/">Visit Website</a>,
+        <a href="https://boldmonkbrewingco.com/">Visit Website</a>,
+        <a href="https://www.theporterbeerbar.com/">Visit Website</a>,
+        <a href="https://www.wreckingbarbrewpub.com/">Visit Website</a>,
+        <a href="https://www.torchedhopbrewing.com/">Visit Website</a>,
+        <a href="https://mondaynightbrewing.com/">Visit Website</a>
       ],
       // Italian
       [
-        "Amalfi Cucina & Mercato",
-        "Pasta Da Pulcinella",
-        "No. 246",
-        "Sotto Sotto",
-        "BoccaLupo",
-        "Mezza Luna",
-        "Argosy",
-        "Ammazza",
-        "Fellini’s Pizza",
-        "Antico Pizza"
+        <a href="https://www.amalfiatl.com/">Visit Website</a>,
+        <a href="https://pastadapulcinella.com/menu">Visit Website</a>,
+        <a href="https://no246.com/">Visit Website</a>,
+        <a href="https://www.sottosottoatl.com/">Visit Website</a>,
+        <a href="http://boccalupoatl.com/">Visit Website</a>,
+        <a href="https://mezzalunaga.com/">Visit Website</a>,
+        <a href="http://argosy-east.com/">Visit Website</a>,
+        <a href="https://www.ammazza.com/">Visit Website</a>,
+        <a href="https://fellinisatlanta.com/">Visit Website</a>,
+        <a href="https://littleitalia.com/">Visit Website</a>
       ],
       // American
       [
-        "Atlanta Breakfast Club",
-        "Sun in My Belly",
-        "Leon’s Full Service",
-        "Argosy",
-        "Folk Art Restaurant"
+        <a href="http://atlantabreakfastclub.com/">Visit Website</a>,
+        <a href="https://www.yelp.com/biz/sun-in-my-belly-atlanta-12">Visit Website</a>,
+        <a href="https://www.leonsfullservice.com/">Visit Website</a>,
+        <a href="https://farmburger.com/menu/">Visit Website</a>,
+        <a href="https://www.folkartrestaurant.com/">Visit Website</a>
       ],
       // Mediterranean
       [
-        "Aviva by Kameel",
-        "Ameer’s Mediterranean Grill",
-        "Baraka Shawarma",
-        "Delbar Middle Eastern",
-        "Sufi’s",
-        "Nick’s Food To Go",
-        "Yalla"
+        <a href="https://www.avivabykameel.com/">Visit Website</a>,
+        <a href="https://ameersmediterranean.com/">Visit Website</a>,
+        <a href="https://www.barakashawarmaatl.com/">Visit Website</a>,
+        <a href="https://www.delbaratl.com/">Visit Website</a>,
+        <a href="https://sufisatlanta.com/">Visit Website</a>,
+        <a href="https://www.nicksfood.com/">Visit Website</a>,
+        <a href="https://www.yallaatl.com/">Visit Website</a>
       ]
     ]
 
